92bf333b399455280a82559c562a6d84d5a397e1
Anthony <anthony.fabregat@gmail.com>
2017-12-22 10:08:16 +0100

    'test'

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
 <?php
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+    <title></title>
+  </head>
+
+  <div id="wrapper">
+  <header>
+    <h1> holistwood </h1>
+  </header>
+
+<main>
+  <body>
+
+    <div id="trailer">
+      <iframe  width="917" height="490" src="https://www.youtube.com/embed/a8v__0kHzNg" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+    </div>
+
+    <div class="affiches">
+      <article class="affiche"> </article>
+      <article class="affiche"> </article>
+      <article class="affiche"> </article>
+      <article class="affiche"> </article>
+
+      <article class="affiche"> </article>
+      <article class="affiche"> </article>
+      <article class="affiche"> </article>
+      <article class="affiche"> </article>
+    </div>
+
+  </body>
+</main>
+
+  <footer>
+
+  </footer>
+</div>
+
+
+</html>
