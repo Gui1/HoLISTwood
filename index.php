64b93e0d064ba87b675f2336772aac98b729fc01
Guillaume Giudici <gui.giudici@gmail.com>
2017-12-22 14:11:43 +0100

    'gui'

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
 <?php
 include_once('./inc/header.php');
 ?>
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
-    <title></title>
-  </head>
 
-  <body>
-
-  <div id="wrapper">
-  <header>
-    <h1> holistwood </h1>
-  </header>
-
-=======
->>>>>>> f648e659c2769128dbd93c9a3e06f8c80c88c2fd
 <main>
 
     <div id="trailer">
@@ -39,15 +21,6 @@ include_once('./inc/header.php');
     </div>
 
 </main>
-
-
-<<<<<<< HEAD
-  </footer>
-  </body>
-</div>
-=======
->>>>>>> f648e659c2769128dbd93c9a3e06f8c80c88c2fd
-
 
 <?php
 include_once('./inc/footer.php');
